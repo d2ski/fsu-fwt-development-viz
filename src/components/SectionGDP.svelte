5bb5292a7b547a5deea268e1f7f2ea41cb1133f5
d2ski <chukotski@gmail.com>
2022-04-26 14:05:50 +0200

    labels shifting

--- src/components/SectionGDP.svelte
+++ src/components/SectionGDP.svelte
@@ -3,20 +3,16 @@
   import data from "$data/GDPPPC.json";
   import SlopeChart from "$components/SlopeChart.svelte";
 
-  const padding = { t: 25, r: 100, b: 25, l: 25 };
+  const padding = { t: 25, r: 125, b: 25, l: 25 };
   const w = 350 - padding.r - padding.l;
   const h = 550 - padding.t - padding.b;
 
   const domainYears = extent(data, (rec) => rec.year);
   const domainValues = extent(data, (rec) => rec.value);
 
-  $: yScale = scaleLinear()
-    .domain(domainValues)
-    .range([h, 0]);
+  $: yScale = scaleLinear().domain(domainValues).range([h, 0]);
 
-  $: xScale = scaleLinear()
-    .domain(domainYears)
-    .range([0, w]);
+  $: xScale = scaleLinear().domain(domainYears).range([0, w]);
 
   const generateLines = function (data, activeGroup) {
     const lines = [...group(data, (rec) => rec.countryCode).values()].map(
@@ -26,27 +22,31 @@
           isActive: rec[0].group === activeGroup,
         };
 
-        if (rec[0].year < rec[1].year) {
-          line.x1 = xScale(rec[0].year);
-          line.x2 = xScale(rec[1].year);
-          line.y1 = yScale(rec[0].value);
-          line.y2 = yScale(rec[1].value);
-        } else {
-          line.x1 = xScale(rec[1].year);
-          line.x2 = xScale(rec[0].year);
-          line.y1 = yScale(rec[1].value);
-          line.y2 = yScale(rec[0].value);
-        }
+        let [first, last] = rec[0].year < rec[1].year ? [0, 1] : [1, 0];
+        line.x1 = xScale(rec[first].year);
+        line.x2 = xScale(rec[last].year);
+        line.y1 = yScale(rec[first].value);
+        line.y2 = yScale(rec[last].value);
 
         return line;
       }
     );
 
-    return lines.sort( (a, b) => a.isActive - b.isActive);
+    return lines.sort((a, b) => a.isActive - b.isActive);
   };
 
-  $: linesFSU = generateLines(data, 'FSU');
+  $: linesFSU = generateLines(data, "FSU");
 
+  const shiftLabelsFSU = {
+    key: "country",
+    shift: [
+      ["Georgia", -4],
+      ["Azerbaijan", 3],
+      ["Armenia", -1.5],
+      ["Ukraine", 6],
+      ["Moldova", 15],
+    ],
+  };
 </script>
 
-<SlopeChart lines={linesFSU} {w} {h} {padding} />
+<SlopeChart lines={linesFSU} {w} {h} {padding} shiftLabels={shiftLabelsFSU} />
--- src/components/SlopeChart.svelte
+++ src/components/SlopeChart.svelte
@@ -5,11 +5,19 @@
   export let w;
   export let h;
   export let padding;
+  export let shiftLabels = [];
 
   $: width = w + padding.r + padding.l;
   $: height = h + padding.t + padding.b;
 
   const labelsPadding = 5;
+  const labels = lines.slice();
+
+  shiftLabels.shift.forEach(shiftLbl => {
+    const lbl = labels.find(el => el[shiftLabels.key] === shiftLbl[0]);
+    lbl.lblPos = lbl.y2 + shiftLbl[1];
+  })
+
 </script>
 
 <div class="chart">
@@ -38,8 +46,8 @@
       class="chart__labels"
       transform={`translate(${padding.l + w + labelsPadding}, ${padding.t})`}
     >
-      {#each lines as { x2, y2, country, isActive }}
-        <text y={y2} class:active={isActive}>{country}</text>
+      {#each labels as { lblPos, y2, country, isActive }}
+        <text y={lblPos || y2} class:active={isActive}>{country}</text>
       {/each}
     </g>
   </svg>
@@ -60,11 +68,20 @@
     stroke: #d50000;
   }
 
+  .chart__lines line {
+    cursor: pointer;
+  }
+
   .chart__labels {
     fill: #e0e0e0;
   }
 
   .chart__labels .active {
     fill: #d50000;
+  }
+
+  .chart__labels text {
+    font-size: 1.25rem;
+    transform: translateY(2px);
   }
 </style>
