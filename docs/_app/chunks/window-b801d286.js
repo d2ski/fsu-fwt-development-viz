9e5d77fc9a6dd403aa811007ff8bc96d66d4e1fb
d2ski <chukotski@gmail.com>
2022-06-02 22:42:30 +0200

    rebuild with new header image path

---
+++ docs/_app/chunks/window-b801d286.js
@@ -0,0 +1,1 @@
+import{w as t}from"./paths-400b4772.js";const s=t(0),a=t(0);export{a as c,s as w};
--- docs/_app/chunks/window-df812a72.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./index-28712744.js";const s=t(0),a=t(0);export{a as c,s as w};
